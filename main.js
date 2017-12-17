23cbd9959e1b1f76bc0ca40de05cdf8541807eeb
Tom Hill <tomhill98@me.com>
2017-12-17 22:46:54 +0700

    Added todos

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+/*
+TODO
+
+[ ] search for word in github
+[ ] load relavent markdown
+[ ] display markdown in tooltip
+[ ] search for keywords in pre and code tags
+    - display underline
+    - allow click and open tooltip
+
+*/
+
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(request.word)
