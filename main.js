67d33fc863cc186cde193f4043a423c7cb80dd1d
Tom Hill <tomhill98@me.com>
2017-12-18 23:30:05 +0700

    creates tooltip over selected text

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ TODO
 [x] search for word in github
 [x] load relavent markdown
 [ ] display markdown in tooltip
+[ ] make tool tip stay in place on scroll
+[ ] flip tooltip based on where the element is on the page
 [ ] search for keywords in pre and code tags
     - display underline
     - allow click and open tooltip
+[ ] comment current codebase
 
 */
 
@@ -38,9 +41,68 @@ function searchTLDR(command, platform) {
   }
 }
 
+
+var div = null
+var tooltip = null
+
 function createTooltip(content) {
+  // https://stackoverflow.com/questions/18302683/how-to-create-tooltip-over-text-selection-without-wrapping/18302723#18302723
+  var selection = window.getSelection(),
+      range = selection.getRangeAt(0),
+      rect = range.getBoundingClientRect();
+
+  if (rect.width >= 0) {
+
+  //   if (div) {
+  //     div.parentNode.removeChild(div)
+  //   }
+  //
+  //   div = document.createElement('div') // create the box
+  //   div.class = 'rect'
+  //   div.style.border = '2px solid black' // create outline
+  //   div.style.position = 'fixed'
+  //   div.style.top = rect.top + 'px'
+  //   div.style.left = rect.left + 'px'
+  //   div.style.height = rect.height + 'px'
+  //   div.style.width = rect.width + 'px'
+  //   document.body.appendChild(div) // append to document
+  // }
+
+    if (tooltip) {
+      tooltip.parentNode.removeChild(tooltip)
+    }
+
+    tooltip = document.createElement('tooltip')
+    Object.assign(
+      tooltip.style,
+      {
+        background: "#4A4A4A",
+        boxShadow: "0 2px 4px 0 rgba(0,0,0,0.50)",
+        borderRadius: "8px",
+        transition: '.2s',
+        position: "absolute",
+
+      }
+    )
+
+    newtop = rect.top - 200 + window.scrollY
+    tooltip.style.top = newtop + 'px'
+    tooltip.style.left = rect.left + 'px'
+    tooltip.style.height = '195px'
+    tooltip.style.width = '300px'
+    document.body.appendChild(tooltip)
+
+  }
 
 }
+
+function removeTooltip() {
+  if (tooltip) {
+    tooltip.parentNode.removeChild(tooltip)
+  }
+}
+
+window.onmouseup = removeTooltip
 
 var commandList = []
 
