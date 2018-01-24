128b4b24184711a5bbaa5e72ba4d2a10847dc23d
pxgamer <owzie123@gmail.com>
2018-01-24 10:18:06 +0000

    Add missing semicolons for ES2015/16 consistency
    In ES2015/16, semicolons are recommended although not required. To
    keep the project consistent, this change adds missing semicolons.

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
 //https://api.github.com/repos/tldr-pages/tldr/contents/pages/common
-let tldrURL = "https://raw.githubusercontent.com/tldr-pages/tldr/master/pages"
-var tooltip = null
-var arrow = null
-var currentContent = null
-console.log(currentContent)
+let tldrURL = "https://raw.githubusercontent.com/tldr-pages/tldr/master/pages";
+var tooltip = null;
+var arrow = null;
+var currentContent = null;
+console.log(currentContent);
 
 // For right-click tldr search
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(request.word)
-    searchTLDR(request.word.toLowerCase())
+    console.log(request.word);
+    searchTLDR(request.word.toLowerCase());
   }
 );
 
@@ -22,11 +22,11 @@ function searchTLDR(command, platform) {
   xhr.open(
     "GET",
     tldrURL + '/' + platform + '/' + command + '.md'
-  )
-  xhr.send()
+  );
+  xhr.send();
 
   function reqListener(){
-    createTooltip(this.responseText)
+    createTooltip(this.responseText);
   }
 
 }
@@ -42,12 +42,12 @@ function createTooltip(content, isMarked) {
   if (rect.width >= 0) {
 
     if (tooltip) {
-      tooltip.parentNode.removeChild(tooltip)
+      tooltip.parentNode.removeChild(tooltip);
     }
 
-    tooltip = document.createElement('div')
-    tooltip.id = "tldr-chrome"
-    newtop = rect.top - 200 + window.scrollY
+    tooltip = document.createElement('div');
+    tooltip.id = "tldr-chrome";
+    newtop = rect.top - 200 + window.scrollY;
 
     Object.assign(
       tooltip.style,
@@ -56,15 +56,15 @@ function createTooltip(content, isMarked) {
         left: rect.left + 'px',
 
       }
-    )
+    );
 
-    document.body.appendChild(tooltip)
+    document.body.appendChild(tooltip);
 
     // Create Arrow
 
-    arrow = document.createElement('div')
-    arrow.id = "tldr-chrome-arrow"
-    document.body.appendChild(arrow)
+    arrow = document.createElement('div');
+    arrow.id = "tldr-chrome-arrow";
+    document.body.appendChild(arrow);
 
     Object.assign(
       arrow.style,
@@ -72,59 +72,59 @@ function createTooltip(content, isMarked) {
         left: (rect.left) + 5 +'px',
         top: newtop + 195 + 'px' // newtop + height of tooltip
       }
-    )
+    );
 
     // Create markdown and append to tooltip
     if (content.trim() === "404: Not Found") {
-      var markdown = "<div class='not-found'><p class='large'>😱</p><p>Page Not Found!</p><p>Submit a pull request to: <a target='_blank' href='https://github.com/tldr-pages/tldr'>https://github.com/tldr-pages/tldr</a></p></div>"
+      var markdown = "<div class='not-found'><p class='large'>😱</p><p>Page Not Found!</p><p>Submit a pull request to: <a target='_blank' href='https://github.com/tldr-pages/tldr'>https://github.com/tldr-pages/tldr</a></p></div>";
     } else if (isMarked) {
-      var markdown = content
+      var markdown = content;
     } else {
-      var markdown = marked(content)
+      var markdown = marked(content);
     }
 
     currentContent = markdown;
 
-    var markdownContent = document.createElement('div')
-    markdownContent.innerHTML = markdown
-    markdownContent.className += 'tldr-chrome'
-    tooltip.appendChild(markdownContent)
+    var markdownContent = document.createElement('div');
+    markdownContent.innerHTML = markdown;
+    markdownContent.className += 'tldr-chrome';
+    tooltip.appendChild(markdownContent);
   }
 
 }
 
 function removeTooltip() {
   if (tooltip != null) {
-    tooltip.parentNode.removeChild(tooltip)
-    tooltip = null
-    arrow.parentNode.removeChild(arrow)
-    arrow = null
-    currentContent = null
+    tooltip.parentNode.removeChild(tooltip);
+    tooltip = null;
+    arrow.parentNode.removeChild(arrow);
+    arrow = null;
+    currentContent = null;
   }
 }
 
-window.onmousedown = removeTooltip
+window.onmousedown = removeTooltip;
 
-var commandList = []
+var commandList = [];
 
 function generateCommandList(callback) {
-  commandList = []
+  commandList = [];
   var xhr = new XMLHttpRequest();
   xhr.addEventListener("load", reqListener);
   xhr.open(
     "GET",
     "https://api.github.com/repos/tldr-pages/tldr/contents/pages/common"
-  )
-  xhr.send()
+  );
+  xhr.send();
 
   function reqListener(){
-    var arr = JSON.parse( this.responseText )
+    var arr = JSON.parse( this.responseText );
 
     for (doc of arr) {
-      commandList.push(doc.name.split('.')[0])
+      commandList.push(doc.name.split('.')[0]);
     }
 
-    callback()
+    callback();
 
   }
 
@@ -132,12 +132,12 @@ function generateCommandList(callback) {
 
 function checkCode() {
   generateCommandList(function(){
-    var preTags = document.getElementsByTagName('pre')
+    var preTags = document.getElementsByTagName('pre');
     for (tag of preTags) {
       for (word of tag.innerText.split(" ")) {
         if (commandList.indexOf(word.toLowerCase()) > -1) {
           // if word is in commandList
-          console.log("FOUND COMMAND: " + word)
+          console.log("FOUND COMMAND: " + word);
         }
       }
     }
@@ -146,8 +146,8 @@ function checkCode() {
 
 window.onresize = function(event) {
   if (tooltip) {
-    oldContent = currentContent
-    removeTooltip()
-    createTooltip(oldContent, true)
+    oldContent = currentContent;
+    removeTooltip();
+    createTooltip(oldContent, true);
   }
-}
+};
